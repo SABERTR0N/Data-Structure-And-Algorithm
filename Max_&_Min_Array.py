872ab089c050a64dabb9b5d98b7a4dd7ce9bad37
SABERTR0N <vedantchamoli7@gmail.com>
2026-04-23 12:19:47 +0530

    updated

--- Max_&_Min_Array.py
+++ Max_&_Min_Array.py
@@ -1,14 +1,24 @@
 def MaxofArray(arr):
     if len(arr) == 1:
         return arr[0]
-    
-    max = arr[0]
 
+    max = arr[0]
+    
     for i in range(len(arr)):
         if arr[i] > max:
             max = arr[i]
-
     return max
+
+def MinofArray(arr):
+    if len(arr) == 1:
+        return arr[0]
+    
+    min = arr[0]
+    for i in range(len(arr)):
+        if arr[i] < min:
+            min = arr[i]
+    return min
+
     
 n = int(input("Enter size: "))
 arr = []
@@ -16,6 +26,7 @@ for i in range(n):
     num = int(input())
     arr.append(num)
 print("Maximum:" , MaxofArray(arr))
+print("Minimum:" , MinofArray(arr))
 
         
 
