c8835585da872a2712194147772deb3e4fd7d469
SABERTR0N <vedantchamoli7@gmail.com>
2026-05-01 10:51:24 +0530

    If Palindrome

---
+++ CheckIfPalindrome.py
@@ -0,0 +1,12 @@
+def If_palindrome(str):
+    rev = ""
+    for ch in str:
+        rev = ch+rev
+    
+    return rev
+Name = str(input("Enter the Name! "))
+if Name == If_palindrome(Name.lower()):
+    print("It is a Palinedrome!! ")
+
+else:
+    print("Not a Palindrome! ")
